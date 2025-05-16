22e40889e3192c71d370c4bf4eb29875e361a1d2
Joel Jebitto <joeljebitto@gmail.com>
2025-05-16 11:04:07 +0530

    cleaned up

--- the_fun_app/src/App.jsx
+++ the_fun_app/src/App.jsx
@@ -1,41 +1,7 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-  return (
-    <>
-      <div className="flex flex-row space-10 ">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo h-60 flex-1" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img
-            src={reactLogo}
-            className="logo h-60 flex-1 react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card space-y-8">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;

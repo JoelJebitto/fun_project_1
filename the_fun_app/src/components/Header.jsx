843a13f7ec43dfca832524c0cb1328297301a53a
MxVaibs <mvaibhav.main@gmail.com>
2025-05-16 21:07:19 +0530

    header changed

--- the_fun_app/src/components/Header.jsx
+++ the_fun_app/src/components/Header.jsx
@@ -66,7 +66,16 @@ export default function Header() {
                   class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
                   aria-current="page"
                 >
-                  Home
+                  AniCards
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                  aria-current="page"
+                >
+                  AniQuotes
                 </a>
               </li>
             </ul>

b72f894df633a9395482b59c398a81f6a08e0ac9
Joel Jebitto <joeljebitto@gmail.com>
2025-05-16 11:13:21 +0530

    -

--- the_fun_app/src/App.jsx
+++ the_fun_app/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header.jsx";
+import Body from "./components/Header.jsx";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
 }
 
 export default App;
